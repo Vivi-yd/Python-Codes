a369bf13ba613e04d7514d30a4d6fd134befc0b2
Vivi YD <vivid321@yahoo.com>
2015-06-22 23:50:11 +1000

    pushing new file for codes from MIT intro to CS course

---
+++ Intro_CS_MIT/problem_set_0.py
@@ -0,0 +1,9 @@
+# Problem #0
+# Vivian
+# Time Spent: 3 mins
+
+# Ask user to enter DOB and Name 
+dob = raw_input("Please enter your Date of Birth (dd/mm/yy): ")
+name = raw_input("Please enter your full name: ")
+print "your date of birth is: " + dob
+print "your name is: " + name
